4ce1995e774528076cc89f227d52dea9e4315dd1
namaomo <63583749+namaomo1694@users.noreply.github.com>
2020-06-08 18:16:30 +0900

    Add files via upload

---
+++ honnda.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# coding:utf-8
+
+import time
+
+from pyfiglet import Figlet
+if __name__ == '__main__':
+    f = Figlet(font='big')
+    msg = f.renderText('YOU LOSE')
+print("2020年もっと美味しくなったペプシジャパンコーラ飲んでみたくないですか？")
+time.sleep(1)
+print("僕にじゃんけんで買ったら")
+time.sleep(1)
+print("一本あげますよ。")
+time.sleep(1)
+print("じゃーんけんぽん")
+time.sleep(2)
+print("あなたは何を出す？")
+time.sleep(1)
+print("グー＝１、チョキ＝２、パー＝３")
+input(">>")
+time.sleep(1)
+print(msg)
+time.sleep(1)
+print("俺の勝ち!")
+time.sleep(1)
+print("何事も準備がすべて")
+time.sleep(1)
+print("それを怠っていることがバレてますよ")
+time.sleep(1)
+print("ほな注ぎます")
+time.sleep(1)
+print("めっちゃ美味い!")
+time.sleep(1)
+print("飲みたい？")
+time.sleep(1)
+print("ほな")
+time.sleep(1)
+print("明日も挑戦")
+
